b32aba68c634da84269c0bbabb902692eda08eaf
aosuikun <ao@suikun.net>
2025-12-28 09:06:48 -0300

    handling creating of environment during first run. Now also using python3 instead of python

--- gzfe.sh
+++ gzfe.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-source ./venv/bin/activate
+if [ -f "./venv/bin/activate" ]; then
+    source ./venv/bin/activate
+else
+    python3 -m venv ./venv
+    source ./venv/bin/activate
+    pip install pygame
+fi
+
 python gzfe.py
